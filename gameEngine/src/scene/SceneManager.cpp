c8588fff9399d6b95d82a6cda0fabc03f2ee079c
raphael <camblong.raphael@epitech.eu>
2023-12-20 23:06:58 +0100

    [ADD] scene Manager GE

---
+++ gameEngine/src/scene/SceneManager.cpp
@@ -0,0 +1,25 @@
+//
+// Created by raphael on 12/20/23.
+//
+
+#include "gameEngine/scene/SceneManager.hpp"
+
+namespace GameEngine::Scene {
+void SceneManager::addScene(std::string const& name, std::unique_ptr<IScene> scene) {
+  auto result = m_scenes.emplace(name, std::move(scene));
+  if (!result.second) {
+    throw std::invalid_argument("Scene with the same name:" + name + "already exists");
+  }
+};
+
+void SceneManager::deleteScene(std::string const& name) {
+  if (!m_scenes.contains(name)) {
+    throw std::invalid_argument("Scene with the name: " + name + " doesn't exists");
+  }
+  m_scenes.erase(name);
+};
+
+IScene& SceneManager::getScene(std::string const& name) { return *m_scenes[name]; };
+
+bool SceneManager::exist(std::string const& name) { return m_scenes.contains(name); };
+}  // namespace GameEngine::Scene
