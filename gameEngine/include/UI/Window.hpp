b7e2c141ff8f87316310c97ac7bb14a22a5b43b3
raphael <camblong.raphael@epitech.eu>
2023-12-20 21:52:13 +0100

    [FIX] ADD window GE UI parent class

--- gameEngine/include/UI/Window.hpp
+++ gameEngine/include/UI/Window.hpp
@@ -4,8 +4,46 @@
 
 #pragma once
 
-namespace GameEngine::UI {
+#include <SFML/Graphics.hpp>
+#include <functional>
+#include <string>
 
-class Window {};
+namespace GameEngine::UI {
+#define DEFAULT_RATIO (16.f / 9.f)
+
+struct WindowContext {
+  sf::RenderWindow window;
+  sf::Event event;
+  sf::Clock deltaClock;
+  float screenRatio;
+};
+
+class Window {
+ public:
+  Window(int width, int height, std::string& name, float screenRatio = DEFAULT_RATIO);
+
+  void setFrameRate(int);
+
+  void subscribeEvent(std::function<void(WindowContext&)>& closure);
+  void unsubscribeEvent();
+
+  void subscribeUpdate(std::function<void(WindowContext&)>& closure);
+  void unsubscribeUpdate();
+
+  void subscribeDisplay(std::function<void(WindowContext&)>& closure);
+  void unsubscribeDisplay();
+
+  void launch();
+  void display();
+
+ private:
+  void handleEvent();
+  void onResize();
+
+  WindowContext m_windowContext;
+  std::function<void(WindowContext&)> m_closureEvent;
+  std::function<void(WindowContext&)> m_closureUpdate;
+  std::function<void(WindowContext&)> m_closureDisplay;
+};
 
 }  // namespace GameEngine::UI
--- gameEngine/src/UI/Window.cpp
+++ gameEngine/src/UI/Window.cpp
@@ -3,5 +3,77 @@
 //
 
 #include "UI/Window.hpp"
+#include
 
-namespace GameEngine::UI {}  // namespace GameEngine::UI
+namespace GameEngine::UI {
+Window::Window(int width, int height, std::string& name, float screenRatio)
+    : m_windowContext{
+          .window = sf::RenderWindow(sf::VideoMode(width, height), name),
+          .event = sf::Event(),
+          .deltaClock = sf::Clock(),
+          .screenRatio = screenRatio,
+      } {}
+
+void Window::setFrameRate(int limit) { m_windowContext.window.setFramerateLimit(limit); }
+
+void Window::subscribeEvent(std::function<void(WindowContext&)>& closure) { m_closureEvent = closure; }
+void Window::unsubscribeEvent() { m_closureEvent = {}; }
+
+void Window::subscribeUpdate(std::function<void(WindowContext&)>& closure) { m_closureEvent = closure; };
+void Window::unsubscribeUpdate() { m_closureUpdate = {}; };
+
+void Window::subscribeDisplay(std::function<void(WindowContext&)>& closure) { m_closureDisplay = closure; };
+void Window::unsubscribeDisplay() { m_closureDisplay = {}; };
+
+void Window::launch() {
+#pragma unroll
+  while (m_windowContext.window.isOpen()) {
+    m_closureUpdate(m_windowContext);
+    this->handleEvent();
+  }
+}
+
+void Window::handleEvent() {
+#pragma unroll
+  while (m_windowContext.window.pollEvent(m_windowContext.event)) {
+    switch (m_windowContext.event.type) {
+      case sf::Event::Closed:
+        return m_windowContext.window.close();
+      case sf::Event::Resized:
+        return this->onResize();
+      default:
+        break;
+    }
+    m_closureEvent(m_windowContext);
+  }
+}
+
+void Window::onResize() {
+  float targetRatio = m_windowContext.screenRatio;
+  float currentRatio = static_cast<float>(m_windowContext.window.getSize().x) / m_windowContext.window.getSize().y;
+  int newWidth = 0;
+  int newHeight = 0;
+
+  if (currentRatio > targetRatio) {
+    newWidth = static_cast<int>(m_windowContext.window.getSize().y * targetRatio);
+    newHeight = m_windowContext.window.getSize().y;
+  } else {
+    newWidth = m_windowContext.window.getSize().x;
+    newHeight = static_cast<int>(m_windowContext.window.getSize().x / targetRatio);
+  }
+
+  int offsetX = static_cast<int>(m_windowContext.window.getSize().x - newWidth) / 2;
+  int offsetY = static_cast<int>(m_windowContext.window.getSize().y - newHeight) / 2;
+
+  sf::View view(sf::FloatRect(0.f, 0.f, static_cast<float>(newWidth), static_cast<float>(newHeight)));
+  m_windowContext.window.setView(view);
+  m_windowContext.window.setPosition(sf::Vector2i(offsetX, offsetY));
+}
+
+void Window::display() {
+  m_windowContext.window.clear();
+  m_closureDisplay(m_windowContext);
+  m_windowContext.window.display();
+};
+
+};  // namespace GameEngine::UI
