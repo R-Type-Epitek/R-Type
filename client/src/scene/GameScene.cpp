8f23ed233b1356b6e9a2f2323f3afb6496390ab3
raphael <camblong.raphael@epitech.eu>
2023-12-24 16:58:37 +0100

    [ADD] serialize constructor

--- client/src/scene/GameScene.cpp
+++ client/src/scene/GameScene.cpp
@@ -6,6 +6,7 @@
 #include "gameEngine/component/Gravity.hpp"
 #include "gameEngine/component/Sprite.hpp"
 #include "gameEngine/component/Transform.hpp"
+#include "gameEngine/component/MetaData.hpp"
 #include "gameEngine/ecs/Registry.hpp"
 #include "gameEngine/ecs/RegistryBuilder.hpp"
 #include "gameEngine/ecs/Signature.hpp"
@@ -34,14 +35,26 @@ namespace Client
     builder.registerComponent<ComponentRType::Sprite>();
     builder.registerComponent<ComponentRType::Transform>();
     builder.registerComponent<ComponentRType::Gravity>();
+    builder.registerComponent<ComponentRType::MetaData>();
+    builder.registerComponent<ComponentRType::MetaDataTest>();
     // Systems
     builder.buildSystemPhysics();
     builder.buildSystemAnimation();
     builder.buildSystemRenderer();
     builder.buildSystemKeyboard();
+    builder.buildSystemEcsSerializer();
     m_registry = builder.getResult();
     // Custom client System
     initCustomSystem();
+  }
+
+  void GameScene::initCustomSystem()
+  {
+    GameEngine::ECS::Signature signature;
+    m_registry->registerSystem<Client::System::Network::Keyboard>();
+    //  System components
+    signature.set(m_registry->getComponentType<ComponentRType::Sprite>());
+    m_registry->setSystemSignature<Client::System::Network::Keyboard>(signature);
   }
 
   void GameScene::initEntities()
@@ -52,6 +65,8 @@ namespace Client
     m_registry->addComponent(entt, ComponentRType::Gravity {Vec3 {1}});
     m_registry->addComponent(entt, ComponentRType::Transform {{0, 0}});
     m_registry->addComponent(entt, ComponentRType::Sprite {});
+    m_registry->addComponent(entt, ComponentRType::MetaData {"test entity 1"});
+    m_registry->addComponent(entt, ComponentRType::MetaDataTest {"test entity 2", 2});
 
     auto entt2 = m_registry->createEntity();
     m_entities.push_back(entt2);
@@ -62,16 +77,8 @@ namespace Client
     auto entt1 = m_registry->createEntity();
     m_entities.push_back(entt1);
     m_registry->addComponent(entt1, ComponentRType::Sprite {});
-  }
-
-  void GameScene::initCustomSystem()
-  {
-    GameEngine::ECS::Signature signature;
-    m_registry->registerSystem<Client::System::Network::Keyboard>();
-    //  System components
-    signature.set(m_registry->getComponentType<ComponentRType::Sprite>());
-    m_registry->setSystemSignature<Client::System::Network::Keyboard>(
-      signature);
+    m_registry->addComponent(entt1, ComponentRType::MetaData {"test entity 2"});
+    m_registry->addComponent(entt1, ComponentRType::MetaDataTest {"test entity 2", 2});
   }
 
 } // namespace Client
