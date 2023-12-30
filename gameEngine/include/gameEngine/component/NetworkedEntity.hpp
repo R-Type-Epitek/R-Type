2d948d1131abf14bb52e146ee28e4afab4dcc734
raphael <camblong.raphael@epitech.eu>
2023-12-30 17:17:48 +0100

    [ADD] Ecs components network identifiers

---
+++ gameEngine/include/gameEngine/component/NetworkedEntity.hpp
@@ -0,0 +1,37 @@
+//
+// Created by raphael on 12/28/23.
+//
+
+#pragma once
+#include <cstddef>
+#include "gameEngine/network/Serializer.hpp"
+
+namespace ComponentRType
+{
+
+  struct NetworkedEntity : public GameEngine::Network::Serializer::BaseNetworkComponent {
+    size_t id;
+
+    NetworkedEntity() = default;
+    explicit NetworkedEntity(size_t id)
+      : id(id)
+    {
+    }
+
+    friend bool operator==(const NetworkedEntity &lhs, const NetworkedEntity &rhs)
+    {
+      return lhs.id == rhs.id;
+    }
+
+  private:
+    friend class boost::serialization::access;
+    template<class Archive>
+    void serialize(Archive &archive, const unsigned int)
+    {
+      archive &boost::serialization::base_object<GameEngine::Network::Serializer::BaseNetworkComponent>(
+        *this);
+      archive & id;
+    }
+  };
+
+} // namespace ComponentRType
