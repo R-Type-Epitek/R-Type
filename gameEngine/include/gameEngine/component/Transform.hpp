6c010c41db05db67345430545c0c9712c4c618ec
raphael <camblong.raphael@epitech.eu>
2024-01-04 18:53:16 +0100

    [ADD] serialization to components

--- gameEngine/include/gameEngine/component/Transform.hpp
+++ gameEngine/include/gameEngine/component/Transform.hpp
@@ -5,7 +5,9 @@
 #pragma once
 
 #include "constants/constants.hpp"
+#include "gameEngine/network/Serializer.hpp"
 
+#include <boost/serialization/base_object.hpp>
 #include <utility>
 
 using Vec3 = int;
@@ -13,15 +15,17 @@ using Vec3 = int;
 namespace ComponentRType
 {
   /// \brief A component representing the transformation properties of an entity.
-  struct Transform {
+  struct Transform : public GameEngine::Network::Serializer::BaseNetworkComponent {
+    friend class boost::serialization::access;
     sf::Vector2f position;
     sf::Vector2f rotation;
     sf::Vector2f speed;
     sf::Vector2f movement;
 
     template<class Archive>
-    void serialize(Archive& ar, unsigned int const version)
+    void serialize(Archive &ar, unsigned int const)
     {
+      ar &boost::serialization::base_object<GameEngine::Network::Serializer::BaseNetworkComponent>(*this);
       ar & position;
       ar & rotation;
       ar & speed;
