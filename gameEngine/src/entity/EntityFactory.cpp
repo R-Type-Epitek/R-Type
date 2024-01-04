d9184d20f4a560f520186c51e6787e837b406115
raphael <camblong.raphael@epitech.eu>
2024-01-04 16:19:10 +0100

    [UPDATE] entity Factory use BluePrint

--- gameEngine/src/entity/EntityFactory.cpp
+++ gameEngine/src/entity/EntityFactory.cpp
@@ -6,6 +6,8 @@
 #include "gameEngine/asset/AssetManager.hpp"
 #include "gameEngine/component/NetworkedEntity.hpp"
 #include "gameEngine/component/MetaData.hpp"
+#include "gameEngine/component/Gravity.hpp"
+#include "gameEngine/component/Clickable.hpp"
 #include "gameEngine/component/Controllable.hpp"
 #include "gameEngine/component/Transform.hpp"
 #include "gameEngine/component/Position.hpp"
@@ -15,80 +17,113 @@
 namespace GameEngine::Entity
 {
 
+  EntityBluePrint EntityFactory::playerEntity {
+    .light = false,
+    .blueprint = {
+      .transform = true,
+      .displayable = true,
+      .controllable = true,
+      .networkedEntity = true,
+      .metaData = true,
+      .position = true,
+      .gravity = true}};
+
+  EntityBluePrint EntityFactory::enemyEntity {
+    .light = false,
+    .blueprint = {
+      .transform = true,
+      .displayable = true,
+      .controllable = true,
+      .networkedEntity = true,
+      .metaData = true,
+      .position = true,
+      .gravity = true}};
+
+  EntityBluePrint EntityFactory::bulletEntity {
+    .light = false,
+    .blueprint = {
+      .transform = true,
+      .displayable = true,
+      .controllable = true,
+      .networkedEntity = true,
+      .metaData = true,
+      .position = true,
+      .gravity = true}};
+
   EntityFactory::EntityFactory(std::vector<GameEngine::ECS::Entity> &entities, ECS::Registry &registry)
     : m_entities(entities)
     , m_registry(registry)
   {
   }
 
-  GameEngine::ECS::Entity EntityFactory::create(const BulletPayload &payload)
+  GameEngine::ECS::Entity EntityFactory::create(const EntityType &type)
   {
-    spdlog::info("[EntityFactory] Creating Bullet entity...");
-    return createBullet(payload);
-  }
-  GameEngine::ECS::Entity EntityFactory::create(const EnemyPayload &payload)
-
-  {
-    spdlog::info("[EntityFactory] Creating Enemy entity...");
-    return createEnemy(payload);
-  };
-
-  GameEngine::ECS::Entity EntityFactory::create(const PlayerPayload &payload)
-  {
-    spdlog::info("[EntityFactory] Creating Player entity...");
-    return createPlayer(payload);
-  };
-
-  void EntityFactory::createFromMetaData(ComponentRType::MetaData &data)
-  {
-    spdlog::info("[EntityFactory] Creating entity from Metadata...");
-  }
-
-  GameEngine::ECS::Entity EntityFactory::createPlayer(PlayerPayload payload)
-  {
-    auto builder = EntityBuilder(m_registry);
-    builder.buildComponent(ComponentRType::MetaData {});
-    builder.buildComponent(ComponentRType::NetworkedEntity {});
-    builder.buildComponent(ComponentRType::Controllable {});
-    builder.buildComponent(ComponentRType::Position {});
-    builder.buildComponent(ComponentRType::Transform {});
-    if (!payload.light) {
-      builder.buildComponent(ComponentRType::Displayable(GameEngine::Asset::getTexture(payload.texturePath)));
+    spdlog::info("[EntityFactory] Creating entity...");
+    switch (type) {
+      case EntityType::Player:
+        spdlog::info("[EntityFactory] Creating Player entity...");
+        return createFromBluePrint(playerEntity);
+      case EntityType::Enemy:
+        spdlog::info("[EntityFactory] Creating Enemy entity...");
+        return createFromBluePrint(enemyEntity);
+      case EntityType::Bullet:
+        spdlog::info("[EntityFactory] Creating Bullet entity...");
+        return createFromBluePrint(bulletEntity);
+      case EntityType::None:
+        throw std::runtime_error("Cannot create entity of type None");
+      default:
+        throw std::runtime_error("Unknown entity type");
     }
+  }
 
-    auto entity = builder.getResult();
-    m_entities.push_back(entity);
+  GameEngine::ECS::Entity EntityFactory::createFromNetwork(
+    ComponentRType::NetworkedEntity &id,
+    ComponentRType::MetaData &metaData)
+  {
+    spdlog::info("[EntityFactory] Creating entity from Network...");
+    auto entity = create(metaData.entityType);
+    auto &componentManager = m_registry.getComponentManager();
+
+    auto &idMem = componentManager->getComponent<ComponentRType::NetworkedEntity>(entity);
+    auto &metaDataMem = componentManager->getComponent<ComponentRType::MetaData>(entity);
+    idMem = id;
+    metaDataMem = metaData;
+
+    auto compDisplay = ComponentRType::Displayable(GameEngine::Asset::getTexture(metaData.texturePath));
+    auto &compDisplayMem = componentManager->getComponent<ComponentRType::Displayable>(entity);
+    compDisplayMem = compDisplay;
+
     return entity;
   }
 
-  GameEngine::ECS::Entity EntityFactory::createEnemy(EnemyPayload payload)
+  GameEngine::ECS::Entity EntityFactory::createFromBluePrint(EntityBluePrint const &payload)
   {
+    spdlog::info("[EntityFactory] Creating entity from BluePrint...");
     auto builder = EntityBuilder(m_registry);
-    builder.buildComponent(ComponentRType::MetaData {});
-    builder.buildComponent(ComponentRType::NetworkedEntity {});
-    builder.buildComponent(ComponentRType::Controllable {});
-    builder.buildComponent(ComponentRType::Position {});
-    builder.buildComponent(ComponentRType::Transform {});
-    if (!payload.light) {
-      builder.buildComponent(ComponentRType::Displayable(GameEngine::Asset::getTexture(payload.texturePath)));
+    if (payload.blueprint.transform) {
+      builder.buildComponent(ComponentRType::Transform {});
     }
-
-    auto entity = builder.getResult();
-    m_entities.push_back(entity);
-    return entity;
-  }
-
-  GameEngine::ECS::Entity EntityFactory::createBullet(BulletPayload payload)
-  {
-    auto builder = EntityBuilder(m_registry);
-    builder.buildComponent(ComponentRType::MetaData {});
-    builder.buildComponent(ComponentRType::NetworkedEntity {});
-    builder.buildComponent(ComponentRType::Position {});
-    builder.buildComponent(ComponentRType::Transform {});
-    if (!payload.light) {
-      builder.buildComponent(ComponentRType::Displayable(GameEngine::Asset::getTexture(payload.texturePath)));
+    if (payload.blueprint.displayable) {
+      builder.buildComponent(ComponentRType::Displayable(GameEngine::Asset::getTexture()));
     }
-    
+    if (payload.blueprint.clickable) {
+      builder.buildComponent(ComponentRType::Clickable {});
+    }
+    if (payload.blueprint.controllable) {
+      builder.buildComponent(ComponentRType::Controllable {});
+    }
+    if (payload.blueprint.networkedEntity) {
+      builder.buildComponent(ComponentRType::NetworkedEntity {});
+    }
+    if (payload.blueprint.metaData) {
+      builder.buildComponent(ComponentRType::MetaData {});
+    }
+    if (payload.blueprint.position) {
+      builder.buildComponent(ComponentRType::Position {});
+    }
+    if (payload.blueprint.gravity) {
+      builder.buildComponent(ComponentRType::Gravity {});
+    }
     auto entity = builder.getResult();
     m_entities.push_back(entity);
     return entity;
