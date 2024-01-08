b1ec5fd5459715268de2168d4dd9659875798408
X-VINCENT <xavier.vincent@epitech.eu>
2024-01-08 16:54:20 +0100

    [ADD] Protocol documentation

--- gameEngine/include/gameEngine/network/Responses.hpp
+++ gameEngine/include/gameEngine/network/Responses.hpp
@@ -7,17 +7,32 @@
 
 #include <cstdint>
 
+/**
+ * @struct ResponseHeader
+ * @brief Structure representing the header of a network message.
+ *
+ * This structure contains the client identifier, command, the identifier of the command,
+ * the length of the data that follows in the message, the status of the command and the
+ * status message.
+ */
 struct ResponseHeader {
-  int clientId;
-  char command[32];
-  int commandId;
-  uint32_t dataLength;
-  int status;
-  char statusMessage[128];
+  int clientId;            ///< Unique identifier for the client.
+  char command[32];        ///< Command type in ASCII format.
+  int commandId;           ///< Unique identifier for the command.
+  uint32_t dataLength;     ///< Length of the following data in bytes.
+  int status;              ///< Status of the command.
+  char statusMessage[128]; ///< Status message of the command.
 };
 
+/**
+ * @struct Response
+ * @brief Structure representing a complete network message.
+ *
+ * This structure combines the ResponseHeader with the actual data payload, allowing
+ * for easy manipulation and transmission of network messages.
+ */
 struct Response {
-  MessageType type = MessageType::Message; ///< Type of the message.
-  ResponseHeader header;
-  char data[];
+  MessageType type = MessageType::Message; ///< Type of the response.
+  ResponseHeader header;                   ///< Header of the response.
+  char data[];                             ///< Variable-length data payload.
 };
