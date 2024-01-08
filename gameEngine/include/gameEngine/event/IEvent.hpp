4ddaabc77d7a5d5cbd1d73f29c45a0f56fd1d861
raphael <camblong.raphael@epitech.eu>
2024-01-08 23:46:22 +0100

    [ADD] event interface

---
+++ gameEngine/include/gameEngine/event/IEvent.hpp
@@ -0,0 +1,13 @@
+//
+// Created by raphael on 1/8/24.
+//
+
+#pragma once
+
+namespace GameEngine::Event
+{
+  class IEvent {
+   public:
+    virtual ~IEvent() = default;
+  };
+} // namespace GameEngine::Event
---
+++ gameEngine/include/gameEngine/event/IEventListener.hpp
@@ -0,0 +1,17 @@
+//
+// Created by raphael on 1/8/24.
+//
+
+#pragma once
+#include "gameEngine/ecs/entity/Entity.hpp"
+#include "gameEngine/event/Events.hpp"
+
+namespace GameEngine::Event
+{
+  class IEventListener {
+   public:
+    virtual ~IEventListener() = default;
+
+    virtual void handleEvent(IEvent &event) = 0;
+  };
+} // namespace GameEngine::Event
