c5dd067f5b3e24c056b211186a7f97e91de5f641
raphael <camblong.raphael@epitech.eu>
2024-01-09 03:38:57 +0100

    [FIX] event shared_ptr

--- gameEngine/include/gameEngine/event/EventRegistry.hpp
+++ gameEngine/include/gameEngine/event/EventRegistry.hpp
@@ -16,14 +16,14 @@ namespace GameEngine::Event
   class EventRegistry {
    public:
     template<typename EventType>
-    void subscribe(IEventListener& listener)
+    void subscribe(std::shared_ptr<IEventListener> listener)
     {
       char const* typeName = typeid(EventType).name();
-      subscribers[typeName].push_back(std::ref(listener));
+      subscribers[typeName].push_back(listener);
     }
 
     template<typename EventType>
-    void unsubscribe(IEventListener& listener)
+    void unsubscribe(std::shared_ptr<IEventListener> listener)
     {
       char const* typeName = typeid(EventType).name();
       auto it = subscribers.find(typeName);
@@ -33,27 +33,28 @@ namespace GameEngine::Event
           std::remove_if(
             listeners.begin(),
             listeners.end(),
-            [&listener](const std::reference_wrapper<IEventListener>& storedListener) {
-              return &listener == &storedListener.get();
+            [&listener](const std::shared_ptr<IEventListener>& storedListener) {
+              return listener.get() == storedListener.get();
             }),
           listeners.end());
       }
     }
 
     template<typename EventType>
-    void publish(EventType& event)
+    void publish(const IEvent& event)
     {
       char const* typeName = typeid(EventType).name();
       auto it = subscribers.find(typeName);
       if (it != subscribers.end()) {
         for (auto& listener : it->second) {
-          event.dispatch(listener);
+          listener->handleEvent(event);
         }
       }
     }
 
    private:
-    std::unordered_map<char const*, std::vector<std::reference_wrapper<IEventListener>>> subscribers;
+    using ListenerList = std::vector<std::shared_ptr<IEventListener>>;
+    std::unordered_map<char const*, ListenerList> subscribers;
   };
 
 } // namespace GameEngine::Event
