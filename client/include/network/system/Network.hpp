05534782669f23886b391ce441c1da9fab296686
raphael <camblong.raphael@epitech.eu>
2024-01-09 04:58:16 +0100

    [ADD] documentation

--- client/include/network/system/Network.hpp
+++ client/include/network/system/Network.hpp
@@ -17,11 +17,8 @@
 namespace Client::System
 {
   /**
+   * @class KeyboardInputHandler
    * @brief System class for handling keyboard events in a networked game environment.
-   *
-   * The Network class is part of the game engine's system and is responsible for
-   * detecting and processing keyboard input, particularly the arrow keys, and
-   * transmitting these inputs through the network system.
    */
   class KeyboardInputHandler
     : public GameEngine::ECS::System
@@ -31,9 +28,8 @@ namespace Client::System
      * @brief Update the system based on keyboard events.
      *
      * This function checks for key press events and sends corresponding commands
-     * (like "UP", "DOWN", "LEFT", "RIGHT") to the server using the network system.
+     * (like "UP", "DOWN", "LEFT", "RIGHT") to the server using the network interface.
      *
-     * @param ctx Reference to the WindowContext, containing event data.
      * @param network Reference to the network system for sending key commands.
      */
     void update(::Client::Network &networkInstance)
@@ -45,6 +41,14 @@ namespace Client::System
       }
     };
 
+    /**
+     * @brief Handle the keyboard input event.
+     *
+     * This function is called when a keyboard input event is received. It adds the
+     * event to the command queue for processing.
+     *
+     * @param eventRaw Reference to the raw keyboard input event.
+     */
     void handleEvent(const GameEngine::Event::IEvent &eventRaw) final
     {
       auto event = dynamic_cast<const GameEngine::Event::EventKeyboardInput &>(eventRaw);
@@ -52,6 +56,7 @@ namespace Client::System
     }
 
    private:
-    std::queue<GameEngine::Event::EventKeyboardInput> commandQueue {};
+    std::queue<GameEngine::Event::EventKeyboardInput>
+      commandQueue {}; ///< Queue for storing keyboard input events.
   };
 } // namespace Client::System::Network
