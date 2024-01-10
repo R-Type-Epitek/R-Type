3ebd139bd07683fd4eb5174bbcfd171f6dc0f066
raphael <camblong.raphael@epitech.eu>
2024-01-10 23:14:40 +0100

    [FIX] network event game

--- server/src/game/Game.cpp
+++ server/src/game/Game.cpp
@@ -1,10 +1,11 @@
 //
 // Created by raphael on 1/10/24.
 //
+#include "gameEngine/system/EcsSerializer.hpp"
+#include "gameEngine/event/Events.hpp"
 #include "game/Game.hpp"
 #include "sceneController/HostGameController.hpp"
 #include "scene/HostGame.hpp"
-#include "gameEngine/system/EcsSerializer.hpp"
 #include <memory>
 
 namespace Server::Game
@@ -33,18 +34,48 @@ namespace Server::Game
 
   std::vector<std::vector<char>> Game::getSerializedEntities()
   {
-    auto& registry = m_coreGE->getCurrentScene().getEcsRegistry();
+    auto &registry = m_coreGE->getCurrentScene().getEcsRegistry();
 
     auto system = registry.getSystem<GameEngine::System::EcsSerializer>();
-    return system->serialise(registry);
+    return system->serialise();
   }
 
   void Game::update(unsigned int df)
   {
+    m_coreGE->tickUpdate(df);
   }
 
   void Game::pushEvent(Event event, Player player)
   {
+    auto &eventManager = m_coreGE->getCurrentScene().getEventRegistry();
+    switch (event) {
+      case Event::Connect:
+        eventManager.publish<GameEngine::Event::NewPlayer>(GameEngine::Event::NewPlayer(player.id));
+        break;
+      case Event::Disconnect:
+        eventManager.publish<GameEngine::Event::DisconnectedPlayer>(
+          GameEngine::Event::DisconnectedPlayer(player.id));
+        break;
+      case Event::Input:
+        switch (player.key) {
+          case GameEngine::Keybinds::Up:
+            eventManager.publish<GameEngine::Event::MoveUp>(GameEngine::Event::MoveUp(player.id));
+            break;
+          case GameEngine::Keybinds::Down:
+            eventManager.publish<GameEngine::Event::Movedown>(GameEngine::Event::Movedown(player.id));
+            break;
+          case GameEngine::Keybinds::Left:
+            eventManager.publish<GameEngine::Event::MoveLeft>(GameEngine::Event::MoveLeft(player.id));
+            break;
+          case GameEngine::Keybinds::Right:
+            eventManager.publish<GameEngine::Event::MoveRight>(GameEngine::Event::MoveRight(player.id));
+            break;
+          default:
+            break;
+        }
+        eventManager.publish<GameEngine::Event::KeyboardInput>(GameEngine::Event::KeyboardInput(player.key));
+        break;
+    }
   }
 
 } // namespace Server
