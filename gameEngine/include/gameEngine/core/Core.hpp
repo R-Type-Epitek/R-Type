9562aa50dc12790c796ad5922ed4b1767508a968
raphael <camblong.raphael@epitech.eu>
2024-01-10 22:28:14 +0100

    [ADD] core update closure

--- gameEngine/include/gameEngine/core/Core.hpp
+++ gameEngine/include/gameEngine/core/Core.hpp
@@ -5,9 +5,11 @@
 #pragma once
 #include <gameEngine/scene/ISceneManager.hpp>
 #include "gameEngine/scene/SceneManager.hpp"
+
 #include <gameEngine/UI/Window.hpp>
 #include <memory>
 #include <utility>
+#include <functional>
 
 namespace GameEngine::Core
 {
@@ -27,6 +29,10 @@ namespace GameEngine::Core
     ~Core() = default;
 
     void run();
+
+    void tickUpdate(unsigned int df);
+
+    void assignUpdateClosure(const std::function<void()>& closure, unsigned int updateRate);
 
     void stop();
 
@@ -54,8 +60,11 @@ namespace GameEngine::Core
    private:
     const std::string m_projectName;
     unsigned int m_ticksPerSecond;
+    std::chrono::steady_clock::time_point m_lastFrameTime;
     GameEngineState m_gameEngineState;
     bool m_graphical;
+    std::function<void()> m_updateExternClosure;
+    unsigned int m_updateClosureRate;
 
     std::shared_ptr<Scene::ISceneManager> m_sceneManager;
     std::shared_ptr<UI::Window> m_gui;
