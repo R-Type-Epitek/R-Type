07c9b711ac775a9a0aefe48cdffd5fd1a0e73e72
raphael <camblong.raphael@epitech.eu>
2024-01-10 22:30:51 +0100

    [UPDATE] import dependencies

--- client/include/network/tools/Logs.hpp
+++ client/include/network/tools/Logs.hpp
@@ -3,50 +3,11 @@
 //
 
 #pragma once
-#include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window/Keyboard.hpp>
-#include <SFML/Window/Window.hpp>
-#include <boost/array.hpp>
-#include <boost/asio.hpp>
-#include <boost/bind.hpp>
-#include <boost/shared_ptr.hpp>
-#include <exception>
-#include <functional>
-#include <iomanip>
-#include <iostream>
-#include <memory>
-#include <optional>
-#include <ostream>
-#include <sstream>
-#include <string>
-#include <thread>
-#include <unordered_map>
-#include <vector>
-
-// Game Engine
-#include "gameEngine/UI/Window.hpp"
-#include "gameEngine/component/Gravity.hpp"
-#include "gameEngine/component/Displayable.hpp"
-#include "gameEngine/component/Transform.hpp"
-#include "gameEngine/ecs/Registry.hpp"
-#include "gameEngine/ecs/RegistryBuilder.hpp"
-#include "gameEngine/ecs/Signature.hpp"
-#include "gameEngine/ecs/component/Component.hpp"
-#include "gameEngine/ecs/component/ComponentManager.hpp"
-#include "gameEngine/ecs/entity/Entity.hpp"
-#include "gameEngine/ecs/entity/EntityManager.hpp"
 #include "gameEngine/network/Commands.hpp"
 #include "gameEngine/network/MessageType.hpp"
 #include "gameEngine/network/Messages.hpp"
 #include "gameEngine/network/Responses.hpp"
 #include "gameEngine/network/Statuses.hpp"
-#include "gameEngine/scene/IScene.hpp"
-#include "gameEngine/scene/SceneManager.hpp"
-#include "gameEngine/system/Animation.hpp"
-#include "gameEngine/system/Keyboard.hpp"
-#include "gameEngine/system/Physics.hpp"
-#include "gameEngine/system/Renderer.hpp"
 #include "spdlog/spdlog.h"
 
 /**
