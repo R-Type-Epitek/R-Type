414cb98af4e2c6fb9a4e63d6559e9b835175fe36
raphael <camblong.raphael@epitech.eu>
2024-01-11 02:29:36 +0100

    [ADD] utility static method to find by id

--- gameEngine/include/gameEngine/component/NetworkedEntity.hpp
+++ gameEngine/include/gameEngine/component/NetworkedEntity.hpp
@@ -4,6 +4,8 @@
 
 #pragma once
 #include "gameEngine/network/Serializer.hpp"
+#include "gameEngine/ecs/entity/Entity.hpp"
+#include "gameEngine/ecs/component/ComponentManager.hpp"
 
 #include <boost/serialization/base_object.hpp>
 #include <cstddef>
@@ -23,6 +25,25 @@ namespace ComponentRType
     friend bool operator==(const NetworkedEntity &lhs, const NetworkedEntity &rhs)
     {
       return lhs.id == rhs.id;
+    }
+
+    static GameEngine::ECS::Entity getEntityByNetworkedId(
+      const size_t &id,
+      const std::set<GameEngine::ECS::Entity> &entities,
+      GameEngine::ECS::ComponentManager &componentManager)
+    {
+      auto it = std::find_if(
+        entities.begin(),
+        entities.end(),
+        [&componentManager, id](const GameEngine::ECS::Entity &entity) {
+          auto currentId = componentManager.getComponent<ComponentRType::NetworkedEntity>(entity);
+          return id == currentId.id;
+        });
+      if (it != entities.end()) {
+        return *it;
+      } else {
+        throw std::out_of_range("Entity not found with the specified ID");
+      }
     }
 
    private:
