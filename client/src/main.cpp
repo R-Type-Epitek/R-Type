555788f7e026c699a0d9c1d21d2eec9edbfe8084
raphael <camblong.raphael@epitech.eu>
2024-01-12 10:42:03 +0100

    Merge with feature/text_and_clickable branch

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -6,10 +6,16 @@
 
 #include <spdlog/spdlog.h>
 
-int main()
+int main(int const ac, char const *const av[])
 {
   spdlog::set_level(spdlog::level::debug);
-  Client::Client client;
+  std::string ip = DEFAULT_IP;
+  std::string port = DEFAULT_PORT;
+  if (ac == 3) {
+    ip = av[1];
+    port = av[2];
+  }
+  Client::Client client(ip, port);
 
   client.run();
 };
