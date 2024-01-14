914be101acb309dacf3abee37e4938013fc29b1d
raphael <camblong.raphael@epitech.eu>
2024-01-14 22:58:40 +0100

    [UPDATE] player can shoot enemy

--- gameEngine/include/gameEngine/component/Health.hpp
+++ gameEngine/include/gameEngine/component/Health.hpp
@@ -6,8 +6,11 @@
 
 namespace ComponentRType
 {
+  static const int kdefaultDamage = 50;
+
   struct Health {
     int value;
     bool isAlive;
+    int damage = kdefaultDamage;
   };
 } // namespace ComponentRType
